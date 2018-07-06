fe2181f0a4a696569e532efc049ab493557072cb
Nafis Sadat <sadatnfs@uw.edu>
2018-07-06 15:52:04 -0700

    Add tests shell

---
+++ bin/run_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+
+for file in tests/*.R
+do
+    printf "Running test file: ${file}"
+    Rscript -e "source(\"$file\")"
+done
+
