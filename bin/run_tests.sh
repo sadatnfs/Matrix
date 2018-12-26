2971e33cac0d3aa921941e8a8c8f419b9e5330fb
Nafis Sadat <sadatnfs@uw.edu>
2018-12-25 17:03:02 -0800

    Drop stupid data.table call

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-
-## Test to see if data.table is picked up
-R -e "library(data.table); sessionInfo()"
-
 for file in tests/*.R
 do
     printf "Running test file: ${file}"
